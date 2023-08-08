79bffb290b59a3807bc1fddba47c268ae11330bf
Nesterov Dmitriy <nesterov.d.a.spb@gmail.com>
2023-08-08 15:45:26 +0300

    Add welcome screen

---
+++ app/src/main/java/com/summer/cocktailbar/presentation/CocktailBar.kt
@@ -0,0 +1,12 @@
+package com.summer.cocktailbar.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.summer.cocktailbar.R
+
+class CocktailBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cocktail_bar)
+    }
+}
